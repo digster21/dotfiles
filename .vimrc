c91070780cf3577b1341580b21b9b6882d4ee895
digster <alvarezdiego4648@gmail.com>
2025-04-11 21:40:53 +1000

    Initial dotfiles commit

---
+++ .vimrc
@@ -0,0 +1,10 @@
+set nocompatible " Disable Vi Compatibility
+set number " Show line numbers
+set relativenumber " Show relative line numbers
+set tabstop=4 " Tab width
+set shiftwidth=4 " Indent width
+set expandtab " Use Spaces instead of tabs 
+set smartindent	" Auto indent
+syntax on " Enable syntax hilighting
+set clipboard=unnamed " Use system clipboard
+
---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+
+
+
+USER_HOME="$HOME"
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo "$DOTFILES_DIR"
+
+# List dotfiles
+dotfiles=(".bashrc" ".vimrc" ".gitconfig" ".DNE")
+
+for file in "${dotfiles[@]}"; do
+    target="$USER_HOME/$file"
+    source="$DOTFILES_DIR/$file"
+
+
+    if [ ! -e "$source" ]; then 
+	echo "Warning: $source does not exist in directory. Skipping."
+	continue
+    fi
+
+    if [ -L "$target" ]; then 
+        echo "Symlink already exists: $target -> $(readlink $target)"
+        contine
+    elif [ -e "$target" ]; then
+        backup="$target.backup.$(date +%s)"
+        echo "Backing up existing file: $target -> $backup"
+        mv "$target" "$backup"
+    fi
+
+    ln -s "$source" "$target"
+    echo "Linked: $target -> $source"
+done
