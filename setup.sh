d0c6fbeef9b3cfa6e50cfb9f6c9f23e16cfa858f
digster21 <88170354+digster21@users.noreply.github.com>
2025-06-02 19:16:35 +1000

    Improve setup script
    Added sections to make logic behaviour more clear.
    Automatic removal of font download once it is installed.
    Automatic installation of tmux and its dependencies.

--- setup.sh
+++ setup.sh
@@ -4,10 +4,19 @@ set -e
 
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-echo "$DOTFILES_DIR"
+echo "Working in: $DOTFILES_DIR"
+
+###############################################################################
+# Link config files
+###############################################################################
+
+DOT_BASHRC=".bashrc"
+DOT_VIMRC=".vimrc"
+DOT_GITCONF=".gitconfig"
+DOT_TMUXCONF=".tmux.conf"
 
 # List dotfile
-dotfiles=(".bashrc" ".vimrc" ".gitconfig" ".tmux.conf")
+dotfiles=("$DOT_BASHRC" "$DOT_VIMRC" "$DOT_GITCONF" "$DOT_TMUXCONF")
 
 for file in "${dotfiles[@]}"; do
     target="$HOME/$file"
@@ -32,29 +41,61 @@ for file in "${dotfiles[@]}"; do
     echo "Linked: $target -> $source"
 done
 
+###############################################################################
+# Install font
+###############################################################################
+
 FONT="JetBrainsMono"
-FONTS_DIR="$HOME/.local/share/fonts"
-FONT_DIR="$FONTS_DIR/$FONT"
+FONT_DIR="$HOME/.local/share/fonts/$FONT"
 FONT_ARCHIVE="$FONT.tar.xz"
 FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$FONT_ARCHIVE"
 
 # Check if font already exists
 if [ ! -d "$FONT_DIR" ]; then
     if ! command -v curl >/dev/null 2>&1; then
-        echo "curl not found. Installing..."
         sudo apt update && sudo apt install -y curl
+        echo "curl installed"
     fi
-
+    
+    # Install font
     curl -OL "$FONT_URL"
 
+    # Extract font
     mkdir -p "$FONT_DIR"
     tar -xf "$FONT_ARCHIVE" -C "$FONT_DIR"
 
     # Reload font cache
     fc-cache -fv
 
-    echo "$FONT Nerd Font installed successfully."
-else
-    echo "$FONT already installed at $FONT_DIR"
+    # Delete font archive
+    rm "$FONT_ARCHIVE"
+
+    echo "$FONT installed at $FONT_DIR"
+fi
+
+###############################################################################
+# Install TMUX + related deps
+###############################################################################
+
+# Install tmux
+if ! command -v tmux >/dev/null 2>& 1; then 
+    sudo apt update && sudo apt install -y tmux
+    echo "tmux installed"
+fi
+
+# Install tmux package manager
+TMUX_TPM_DIR="$HOME/.tmux/plugins/tpm"
+TMUX_TPM_URL="git@github.com:tmux-plugins/tpm.git"
+
+if [ ! -d "$TMUX_TPM_DIR" ]; then
+    mkdir -p "$TMUX_TPM_DIR"
+    git clone "$TMUX_TPM_URL" "$TMUX_TPM_DIR"
+        
+    echo "TMUX TPM installed"
+fi
+
+# Start tmux with config file
+if [ -f "$HOME/$DOT_TMUXCONF" ]; then 
+    echo "Set TMUX source file with 'tmux source-file $HOME/$DOT_TMUXCONF'"
 fi
 
