82b35f56a70666374d017e7ba3796562dfd5d4a1
digster <alvarezdiego4648@gmail.com>
2025-06-06 19:35:16 +1000

    log progress on jlink installer - may leave it here though

--- install-segger-jlink.sh
+++ install-segger-jlink.sh
@@ -2,15 +2,23 @@
 
 set -e
 
-
-
-
+JLINK_DEB="SEGGER_JLink.deb"
 
 if [[ "$(uname -m)" == "x86_64" ]]; then
-    wget "https://www.segger.com/downloads/jlink/JLink_Linux_x86_64.deb"
+    wget -o "$JLINK_DEB" "https://www.segger.com/downloads/jlink/JLink_Linux_x86_64.deb"
 
-    echo "Installed J-Link (x86_64)"
-    exit 0
+    sudo dpkg -i "$JLINK_DEB"
+
+    sudo apt-get install -f -y
+
+    rm "$JLINK_DEB"
+
+    if command -v JLinkExe >/dev/null 2>&1; then    
+        echo "Installed J-Link (x86_64)"
+    else
+        echo "J-Link installation failed: JLinkExe not found"
+    fi
+else
+    echo "J-Link installation not supported"
 fi
 
-echo "J-Link installation not supported"
