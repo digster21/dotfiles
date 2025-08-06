1182ea082c6da9ac53137e302b818ae6e7dde14c
digster <alvarezdiego4648@gmail.com>
2025-08-06 21:50:49 +1000

    Add nvim neo tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,6 +22,16 @@ require("lazy").setup({
         "nvim-telescope/telescope.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
     },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-tree/nvim-web-devicons",
+            lazy = false,
+        },
+    },
     "folke/tokyonight.nvim",
 })
 
@@ -33,16 +43,29 @@ vim.opt.clipboard = "unnamedplus"
 vim.cmd("syntax on")
 
 -- Remap escape for insert, visual, and select mode
-vim.keymap.set("i", "jk", "<Esc>")
-vim.keymap.set("v", "jk", "<Esc>")
-vim.keymap.set("s", "jk", "<Esc>")
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("v", "jk", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("s", "jk", "<Esc>", { noremap = true, silent = true })
 
--- Telescope mapping
+-- Telescope config
 local telescope = require("telescope.builtin")
-vim.keymap.set("n", "<leader>fa", telescope.find_files, { desc = "Telescope find file by name (all)" })
-vim.keymap.set("n", "<leader>fg", telescope.git_files, { desc = "Telescope find file by name (git ls-files)" })
+vim.keymap.set("n", "<leader>f", telescope.git_files, { desc = "Telescope find file by name (git ls-files)", noremap = true, silent = true }) 
 
--- Tree sitter config
+-- NeoTree config
+require("neo-tree").setup({
+    close_if_last_window = true,
+    filesystem = {
+        filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+        },
+    },
+})
+vim.keymap.set("n", "<leader>t", ":Neotree filesystem toggle<CR>", { desc = "Neotree Toggle filesystem tree", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>g", ":Neotree git_status toggle<CR>", { desc = "Neotree Toggle git status tree", noremap = true, silent = true })
+
+-- Treesitter config
 require("nvim-treesitter.configs").setup({
     highlight = { enabled = true },
     indent = { enabled = true },
