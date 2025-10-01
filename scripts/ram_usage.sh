285dda05c93cd27509b21de89663fd4be61558aa
digster <alvarezdiego4648@gmail.com>
2025-10-01 13:38:42 +0100

    [SCRIPTS] [TMUX] Fix compatibility of ram_usage and CPU temp on different systems

--- scripts/ram_usage.sh
+++ scripts/ram_usage.sh
@@ -6,7 +6,7 @@ avail_kb=$(echo "$meminfo" | awk '/MemAvailable:/ {print $2}')
 used_kb=$((total_kb - avail_kb))
 
 humanize() {
-    printf "$1" "$(echo "$2/1048576" | bc -l)"
+    awk -v fmt="$1" -v v="$2" 'BEGIN {printf fmt, v/1048576}'
 }
 
 used_h=$(humanize "%5.1f" "$used_kb")
