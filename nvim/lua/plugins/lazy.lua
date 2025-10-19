e3bf5043abc02b027b13b014f180c98948488418
digster <alvarezdiego4648@gmail.com>
2025-10-19 19:11:43 +1100

    [NVIM] Add plugin autopairs

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -1,18 +1,19 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { import = "plugins.treesitter" },
-  { import = "plugins.telescope" },
-  { import = "plugins.neotree" },
-  { import = "plugins.cmp" },
-  { import = "plugins.theme" },
-  { import = "plugins.sensible" },
+    { import = "plugins.treesitter" },
+    { import = "plugins.telescope" },
+    { import = "plugins.neotree" },
+    { import = "plugins.cmp" },
+    { import = "plugins.theme" },
+    { import = "plugins.sensible" },
+    { import = "plugins.autopairs" },
 })
