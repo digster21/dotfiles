539df06fc919ed2466230b50e9e135e90fce8317
digster <alvarezdiego4648@gmail.com>
2025-10-19 12:43:07 +1100

    [NVIM] Refactor NVIM Config

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,36 @@
+return {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+  },
+  config = function()
+    local cmp = require("cmp")
+
+    cmp.setup({
+      mapping = cmp.mapping.preset.insert({
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<TAB>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+      }),
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+        { name = "path" },
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      completion = {
+        keyword_length = 2,
+      },
+      formatting = {
+        max_item_count = 10,
+      },
+    })
+  end,
+}
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    highlight = { enable = true },
+    indent = { enable = true },
+    ensure_installed = {
+      "c", "cpp", "lua", "python", "bash", "json", "yaml", "vim", "markdown",
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
+}
