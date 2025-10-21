2115012205d5b544af148f0e733eaa3803c6797e
digster <alvarezdiego4648@gmail.com>
2025-10-21 23:50:47 +1100

    [NVIM] Format lua files + move lsp aggregator file

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,44 +1,43 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local telescope = require("telescope.builtin")
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        local telescope = require("telescope.builtin")
 
-    vim.keymap.set("n", "<leader>fa", function()
+        vim.keymap.set("n", "<leader>fa", function()
             telescope.find_files({ hidden = true })
         end, {
-      desc = "Find files by name",
-    })
+            desc = "Find files by name",
+        })
 
-    vim.keymap.set("n", "<leader>ff", function()
+        vim.keymap.set("n", "<leader>ff", function()
             telescope.git_files({ hidden = true, show_untracked = true })
         end, {
-      desc = "Find git files by name",
-    })
+            desc = "Find git files by name",
+        })
 
-    vim.keymap.set("n", "<leader>fr", function()
-      telescope.live_grep({
-        additional_args = function()
-          return { "--hidden", "--no-ignore" }
-        end,
-      })
-    end, {
-      desc = "Find content by regex (ripgrep)",
-    })
+        vim.keymap.set("n", "<leader>fr", function()
+            telescope.live_grep({
+                additional_args = function()
+                    return { "--hidden", "--no-ignore" }
+                end,
+            })
+        end, {
+            desc = "Find content by regex (ripgrep)",
+        })
 
-    vim.keymap.set("n", "<leader>fs", function()
-      telescope.live_grep({
-        additional_args = function()
-          return { "--hidden", "--no-ignore", "--fixed-strings" }
-        end,
-      })
-    end, {
-      desc = "Find content by string (ripgrep)",
-    })
+        vim.keymap.set("n", "<leader>fs", function()
+            telescope.live_grep({
+                additional_args = function()
+                    return { "--hidden", "--no-ignore", "--fixed-strings" }
+                end,
+            })
+        end, {
+            desc = "Find content by string (ripgrep)",
+        })
 
-    vim.keymap.set("n", "<leader>fw", telescope.grep_string, {
-      desc = "Find content by word under cursor",
-    })
-  end,
+        vim.keymap.set("n", "<leader>fw", telescope.grep_string, {
+            desc = "Find content by word under cursor",
+        })
+    end,
 }
-
