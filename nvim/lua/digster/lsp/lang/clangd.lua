10f540d848cf3a2f350f069737c1a3a6f90b02c2
digster <alvarezdiego4648@gmail.com>
2025-10-22 22:28:12 +1100

    [NVIM] Add lsp support for clangd

---
+++ nvim/lua/digster/lsp/lang/clangd.lua
@@ -0,0 +1,16 @@
+local utils = require("digster.utils")
+
+
+local cmd = { "clangd", "--clang-tidy" }
+local compile_cmd_dir = utils.find_file_dir("compile_commands.json", vim.loop.cwd(), 6)
+
+if compile_cmd_dir then
+    table.insert(cmd, "--compile-commands-dir=" .. compile_cmd_dir)
+end
+
+vim.lsp.config.clangd = {
+    root_markers = { ".git" },
+    cmd = cmd,
+}
+
+vim.lsp.enable("clangd")
