b076e51d6652761808dd40da88c03e29fbacce5a
digster <alvarezdiego4648@gmail.com>
2025-10-22 21:52:25 +1100

    [NVIM] Create utils file for common lua functions

--- nvim/lua/digster/plugins/telescope.lua
+++ nvim/lua/digster/plugins/telescope.lua
@@ -1,22 +1,24 @@
+local utils = require("digster.utils")
+
 return {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         local telescope = require("telescope.builtin")
 
-        vim.keymap.set("n", "<leader>fa", function()
+        utils.keymap_set("n", "<leader>fa", function()
             telescope.find_files({ hidden = true })
         end, {
             desc = "Find files by name",
         })
 
-        vim.keymap.set("n", "<leader>ff", function()
+        utils.keymap_set("n", "<leader>ff", function()
             telescope.git_files({ hidden = true, show_untracked = true })
         end, {
             desc = "Find git files by name",
         })
 
-        vim.keymap.set("n", "<leader>fr", function()
+        utils.keymap_set("n", "<leader>fr", function()
             telescope.live_grep({
                 additional_args = function()
                     return { "--hidden", "--no-ignore" }
@@ -26,7 +28,7 @@ return {
             desc = "Find content by regex (ripgrep)",
         })
 
-        vim.keymap.set("n", "<leader>fs", function()
+        utils.keymap_set("n", "<leader>fs", function()
             telescope.live_grep({
                 additional_args = function()
                     return { "--hidden", "--no-ignore", "--fixed-strings" }
@@ -36,7 +38,7 @@ return {
             desc = "Find content by string (ripgrep)",
         })
 
-        vim.keymap.set("n", "<leader>fw", telescope.grep_string, {
+        utils.keymap_set("n", "<leader>fw", telescope.grep_string, {
             desc = "Find content by word under cursor",
         })
     end,
