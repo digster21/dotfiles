2cabb6c6898e10fde109805c4ec018d0c1cacb6e
digster <alvarezdiego4648@gmail.com>
2026-01-28 21:49:48 +1100

    feat: Make copilot lazy and add keymaps

--- nvim/lua/digster/lazy/plugins/copilotchat.lua
+++ nvim/lua/digster/lazy/plugins/copilotchat.lua
@@ -1,6 +1,12 @@
 return {
     {
         "CopilotC-Nvim/CopilotChat.nvim",
+        event = "VeryLazy",
+        cmd = "CopilotChat",
+        keys = {
+            { "<leader>a", "<cmd>CopilotChat<cr>" },
+            { "<leader>A", "<cmd>CopilotChatModels<cr>" },
+        },
         dependencies = {
             { "nvim-lua/plenary.nvim", branch = "master" },
         },
