2469cea6c3a1b4ffd75e36a26d146667bb9dcd55
digster <alvarezdiego4648@gmail.com>
2026-02-14 00:03:07 +1100

    feat: change descriptions

--- nvim/lua/digster/lazy/plugins/grugfar.lua
+++ nvim/lua/digster/lazy/plugins/grugfar.lua
@@ -6,7 +6,7 @@ return {
         require("grug-far").setup({})
 
         utils.keymap_set("n", "<leader>z", "<cmd>GrugFar<cr>", { desc = "Open Grug far" })
-        utils.keymap_set("v", "<leader>z", "<cmd>'<,'>GrugFar<cr>", { desc = "Open Grug far (multiline search)" })
-        utils.keymap_set("v", "<leader>Z", "<cmd>'<,'>GrugFarWithin<cr>", { desc = "Open Grug far (search within)" })
+        utils.keymap_set("v", "<leader>z", "<cmd>'<,'>GrugFar<cr>", { desc = "Open Grug far (search selection)" })
+        utils.keymap_set("v", "<leader>Z", "<cmd>'<,'>GrugFarWithin<cr>", { desc = "Open Grug far (search within selection)" })
     end,
 }
