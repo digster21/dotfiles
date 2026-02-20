2c989c4f940c4af7a7910f7ee49a242705f8729a
digster <alvarezdiego4648@gmail.com>
2026-02-20 23:21:51 +1100

    [NVIM] Fix grugfar visual mode keymaps

--- nvim/lua/digster/lazy/plugins/grugfar.lua
+++ nvim/lua/digster/lazy/plugins/grugfar.lua
@@ -6,7 +6,7 @@ return {
         require("grug-far").setup({})
 
         utils.keymap_set("n", "<leader>z", "<cmd>GrugFar<cr>", { desc = "Open Grug far" })
-        utils.keymap_set("v", "<leader>z", "<cmd>'<,'>GrugFar<cr>", { desc = "Open Grug far (search selection)" })
-        utils.keymap_set("v", "<leader>Z", "<cmd>'<,'>GrugFarWithin<cr>", { desc = "Open Grug far (search within selection)" })
+        utils.keymap_set("v", "<leader>z", ":GrugFar<cr>", { desc = "Open Grug far (search selection)" })
+        utils.keymap_set("v", "<leader>Z", ":GrugFarWithin<cr>", { desc = "Open Grug far (search within selection)" })
     end,
 }
