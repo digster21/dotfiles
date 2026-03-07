acf421286d53284aa04e2649fc0cff3ddaa5d26d
digster <alvarezdiego4648@gmail.com>
2026-03-07 17:42:30 +1100

    [NVIM] Allow clangd lsp to mount without workspace

--- nvim/lua/digster/lsp/lang/clangd.lua
+++ nvim/lua/digster/lsp/lang/clangd.lua
@@ -1,6 +1,4 @@
 vim.lsp.config.clangd = {
-    root_markers = { "compile_commands.json" },
-    workspace_required = true,
     filetypes = { "c", "cpp", "h", "hpp" },
     cmd = { "clangd", "--clang-tidy" },
 }
