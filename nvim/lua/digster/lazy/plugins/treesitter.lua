cc1c0c8a87fb71d823670d56bf6bcb77881d145b
digster <alvarezdiego4648@gmail.com>
2026-03-15 21:18:17 +1100

    [NVIM] Add treesitter text-objs for func naving

--- nvim/lua/digster/lazy/plugins/treesitter.lua
+++ nvim/lua/digster/lazy/plugins/treesitter.lua
@@ -1,39 +1,66 @@
+local utils = require("digster.utils")
+
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-        highlight = { enable = true },
-        indent = { enable = true },
-        ensure_installed = {
-            "bash",
-            "c",
-            "cpp",
-            "cmake",
-            "diff",
-            "html",
-            "javascript",
-            "jsdoc",
-            "json",
-            "jsonc",
-            "lua",
-            "luadoc",
-            "luap",
-            "markdown",
-            "markdown_inline",
-            "rst",
-            "printf",
-            "python",
-            "query",
-            "regex",
-            "toml",
-            "tsx",
-            "typescript",
-            "vim",
-            "vimdoc",
-            "xml",
-            "yaml",
-            "csv",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {
+            highlight = { enable = true },
+            indent = { enable = true },
+            ensure_installed = {
+                "bash",
+                "c",
+                "cpp",
+                "cmake",
+                "diff",
+                "html",
+                "javascript",
+                "jsdoc",
+                "json",
+                "jsonc",
+                "lua",
+                "luadoc",
+                "luap",
+                "markdown",
+                "markdown_inline",
+                "rst",
+                "printf",
+                "python",
+                "query",
+                "regex",
+                "toml",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "xml",
+                "yaml",
+                "csv",
+            },
         },
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        branch = "main",
+        config = function()
+            local ts_to = require("nvim-treesitter-textobjects.move")
+
+            utils.keymap_set({ "n", "x", "o" }, "]m", function()
+                ts_to.goto_next_start("@function.outer", "textobjects")
+            end, { desc = "Go to next function start" })
+
+            utils.keymap_set({ "n", "x", "o" }, "[m", function()
+                ts_to.goto_previous_start("@function.outer", "textobjects")
+            end, { desc = "Go to prev function start" })
+
+            utils.keymap_set({ "n", "x", "o" }, "]M", function()
+                ts_to.goto_next_end("@function.outer", "textobjects")
+            end, { desc = "Go to next function end" })
+
+            utils.keymap_set({ "n", "x", "o" }, "[M", function()
+                ts_to.goto_previous_end("@function.outer", "textobjects")
+            end, { desc = "Go to prev function end" })
+        end,
     },
 }
