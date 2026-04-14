e192bcd1bcb52ac4578ca292c808de373b6493f2
digster <alvarezdiego4648@gmail.com>
2026-04-14 22:52:29 +1000

    [NVIM] Stop clangd lsp from inserting full prototype on completion

--- nvim/lua/digster/lsp/lang/clangd.lua
+++ nvim/lua/digster/lsp/lang/clangd.lua
@@ -1,6 +1,6 @@
 vim.lsp.config.clangd = {
     filetypes = { "c", "cpp", "h", "hpp" },
-    cmd = { "clangd", "--clang-tidy" },
+    cmd = { "clangd", "--clang-tidy", "--function-arg-placeholders=0" },
 }
 
 vim.lsp.enable("clangd")
