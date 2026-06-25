b1e579070708f4374aa733e1313c962b63f052bc
digster <alvarezdiego4648@gmail.com>
2026-06-25 21:40:16 +1000

    [NVIM] Detach fromatting from LSP integration

--- nvim/lua/digster/lazy/plugins/conform.lua
+++ nvim/lua/digster/lazy/plugins/conform.lua
@@ -3,12 +3,30 @@ return {
     opts = {
         formatters_by_ft = {
             python = { "black" },
-            markdown = { "prettier" },
             sh = { "beautysh" },
+            bash = { "beautysh" },
+            zsh = { "beautysh" },
+            yaml = { "prettier" },
+            markdown = { "prettier" },
             javascript = { "prettier" },
             typescript = { "prettier" },
             typescriptreact = { "prettier" },
             javascriptreact = { "prettier" },
+        },
+        default_format_opts = {
+            lsp_format = "fallback",
+        },
+        formatters = {
+            prettier = {
+                prepend_args = function(_, ctx)
+                    local bo = vim.bo[ctx.buf]
+                    local sw = bo.shiftwidth > 0 and bo.shiftwidth or bo.tabstop
+                    return {
+                        "--tab-width", tostring(sw),
+                        "--use-tabs", tostring(not bo.expandtab),
+                    }
+                end,
+            },
         },
     },
 }
--- nvim/lua/digster/lsp/common.lua
+++ nvim/lua/digster/lsp/common.lua
@@ -1,50 +1,36 @@
 local utils = require("digster.utils")
 
-local function set_global_keymaps(client, buffer)
-    utils.keymap_set("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename symbol" })
-    utils.keymap_set("n", "<leader>h", vim.lsp.buf.hover, { desc = "Hover docs" })
-
-    utils.keymap_set("n", "<leader>v", function()
-        local conform = require("conform")
-
-        local conform_fmtrs = conform.formatters_by_ft[vim.bo.filetype]
-
-        if conform_fmtrs and #conform_fmtrs > 0 then
-            conform.format()
-        else
-            vim.lsp.buf.format()
-        end
-    end, { buffer = buffer, desc = "Format buffer" })
-end
-
-local function configure_diagnostics()
-    vim.diagnostic.config({
-        virtual_text = { current_line = true },
-        underline = true,
-        update_in_insert = true,
-        severity_sort = true,
-        signs = {
-            text = {
-                [vim.diagnostic.severity.ERROR] = "",
-                [vim.diagnostic.severity.WARN] = "",
-                [vim.diagnostic.severity.INFO] = "",
-                [vim.diagnostic.severity.HINT] = "",
-            },
+vim.diagnostic.config({
+    virtual_text = { current_line = true },
+    underline = true,
+    update_in_insert = true,
+    severity_sort = true,
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.INFO] = "",
+            [vim.diagnostic.severity.HINT] = "",
         },
-        float = {
-            border = "rounded",
-            source = "if_many",
-        },
+    },
+    float = {
+        border = "rounded",
+        source = "if_many",
+    },
+})
+
+utils.keymap_set("n", "<leader>v", function()
+    require("conform").format({
+        bufnr = vim.api.nvim_get_current_buf(),
+        async = true,
     })
-end
+end, { desc = "Format buffer" })
 
 vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("digster.lsp", { clear = true }),
-    callback = function(args)
-        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-        local bufnr = args.buf
-        set_global_keymaps(client, bufnr)
-        configure_diagnostics()
+    callback = function()
+        utils.keymap_set("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename symbol" })
+        utils.keymap_set("n", "<leader>h", vim.lsp.buf.hover, { desc = "Hover docs" })
     end,
 })
 
